31c22cf0216a58402ffc61bcce1952a002562b72
Truntaler <truntaler@gmail.com>
2021-01-30 20:15:07 +0100

    made unfinished server class

--- server/server.py
+++ server/server.py
@@ -1,1 +1,15 @@
 import socket
+
+class GameObject:
+    def __init__(self,x,y,w,h,c):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h 
+        self.c = c
+
+
+
+class Player(GameObject):
+    def __init__(self, x, y, w, h, c):
+        super().__init__(x, y, w, h, c)
